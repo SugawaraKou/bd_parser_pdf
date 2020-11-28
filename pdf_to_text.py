7b03d0461dbab3d4f96380efbb287054924d80ac
Sugawrakou <sugawarakoushi01@gmail.com>
2020-11-28 13:40:21 +0300

    pdf to text update

---
+++ pdf_to_text.py
@@ -0,0 +1,63 @@
+import os
+import time
+
+start = 15
+end = 10
+os.system("pdftotext -f " + str(start) + " -l " + str(end) + " -layout pdf/AL_Faction_Guide_RUS.pdf")
+time.sleep(10)
+print("Parsing pdf/AL_Faction_Guide_RUS.pdf: OK")
+start = 15
+end = 10
+os.system("pdftotext -f " + str(start) + " -l " + str(end) + " -layout pdf/Bestiary.pdf")
+time.sleep(10)
+print("Parsing pdf/Bestiary.pdf: OK")
+start = 15
+end = 10
+os.system("pdftotext -f " + str(start) + " -l " + str(end) + " -layout pdf/Eberron.pdf")
+time.sleep(10)
+print("Parsing pdf/Eberron.pdf: OK")
+start = 15
+end = 10
+os.system("pdftotext -f " + str(start) + " -l " + str(end) + " -layout pdf/Faerнаn_faction_guide.pdf")
+time.sleep(10)
+print("Parsing pdf/Faerнаn_faction_guide.pdf: OK")
+start = 15
+end = 10
+os.system("pdftotext -f " + str(start) + " -l " + str(end) + " -layout pdf/Master's_Guide.pdf")
+time.sleep(10)
+print("Parsing pdf/Master's_Guide: OK")
+start = 15
+end = 10
+os.system("pdftotext -f " + str(start) + " -l " + str(end) + " -layout pdf/Player's_guide.pdf")
+time.sleep(10)
+print("Parsing pdf/Player's_guide.pdf: OK")
+start = 15
+end = 10
+os.system("pdftotext -f " + str(start) + " -l " + str(end) + " -layout pdf/Player_book.pdf")
+time.sleep(10)
+print("Parsing pdf/Player_book.pdf: OK")
+start = 15
+end = 10
+os.system("pdftotext -f " + str(start) + " -l " + str(end) + " -layout pdf/Rule_book.pdf")
+time.sleep(10)
+print("Parsing pdf/Rule_book.pdf: OK")
+start = 15
+end = 10
+os.system("pdftotext -f " + str(start) + " -l " + str(end) + " -layout pdf/The_Adventurer's_Guide_to_the_Sword_Coast.pdf")
+time.sleep(10)
+print("Parsing pdf/The_Adventurer's_Guide_to_the_Sword_Coast.pdf: OK")
+start = 15
+end = 10
+os.system("pdftotext -f " + str(start) + " -l " + str(end) + " -layout pdf/Tom_mordenkainen_on_enemies.pdf")
+time.sleep(10)
+print("Parsing pdf/Tom_mordenkainen_on_enemies.pdf: OK")
+start = 15
+end = 10
+os.system("pdftotext -f " + str(start) + " -l " + str(end) + " -layout pdf/Volo's_guide_to_monsters.pdf")
+time.sleep(10)
+print("Parsing pdf/Volo's_guide_to_monsters.pdf: OK")
+start = 15
+end = 10
+os.system("pdftotext -f " + str(start) + " -l " + str(end) + " -layout pdf/Zanatar_leader_about_everything.pdf")
+time.sleep(10)
+print("Parsing pdf/Zanatar_leader_about_everything.pdf.pdf: OK")
